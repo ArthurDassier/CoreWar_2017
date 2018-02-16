764b94573ae12f266d791e5a49ecb9ab01adfc6e
Leo Smith <k0oo@icloud.com>
2018-02-16 11:53:25 +0100

    added main file and simple hello world template in asm

---
+++ asm/include/corewar.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2017
+** corewar.h
+** File description:
+** corewar header file
+*/
+
+#ifndef COREWAR_H_
+#define COREWAR_H_
+
+#include "my.h"
+
+#endif
---
+++ asm/srcs/main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2017
+** main.c
+** File description:
+** <..>
+*/
+
+#include "corewar.h"
+
+int main(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	my_printf("HelloWorld\n");
+	return (0);
+}
