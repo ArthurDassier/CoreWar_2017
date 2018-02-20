08242d6efddf06621262ee7ddcef7c5a04a92ceb
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-20 14:59:52 +0100

    Error gest improved / dumb_nbr_cycle verified in hexa

--- corewar/srcs/args_errors/err_gs_acav.c
+++ corewar/srcs/args_errors/err_gs_acav.c
@@ -10,10 +10,14 @@
 
 int ar_er(int ac, char **av)
 {
-	(void) av;
 	if (check_ac_nb(ac) == 84)
 		return (84);
 	if (check_one_ac(ac) == 84)
 		return (84);
+	if (g_flag(av[1]) == 84)
+		return (84);
+	if (av[1][0] == '-')
+		if (hexa_gest(av[1]) == 84)
+			return (84);
 	return (0);
 }
