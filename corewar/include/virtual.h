4185becaca0018b124e72e7097e5ec19a797045a
Vincent <vincent.lemesle@epitech.eu>
2018-02-27 13:30:57 +0100

    circular memory added

--- corewar/include/virtual.h
+++ corewar/include/virtual.h
@@ -8,8 +8,27 @@
 #ifndef VIRTUAL_H_
 #define VIRTUAL_H_
 
+typedef struct circular_memory
+{
+	char	*memory;
+	char	*memory_end;
+	char	*head;
+	char	*tail;
+	int	count;
+	int	size;
+} circular_memory;
+
 void fill_arg(arguments *argv, char *flag, char *parameter);
 char *init_vm(char **av, arguments *argv);
 int cycles(char *memory_zone, arguments *argv);
+
+int memory_init(circular_memory *vm, int size);
+void memory_put(circular_memory *vm, char data);
+int is_memory_full(circular_memory *vm);
+void memory_pop(circular_memory *vm);
+void memory_free(circular_memory *vm);
+
+#define	FULL		(1)
+#define	NOT_FULL	(0)
 
 #endif
--- corewar/srcs/main.c
+++ corewar/srcs/main.c
@@ -22,12 +22,55 @@ int virtual_machine(char **av)
 int main(int ac, char **av)
 {
 	instructions	**list = malloc(sizeof(instructions) * ac);
+	circular_memory	*vm = malloc(sizeof(circular_memory));
 
 	if (ar_er(ac, av) > 0)
 		return (84);
 	if (init_data_base(list, ac, av) == 84)
 		return (84);
-	if (virtual_machine(av) == 84)
-		return (84);
+	printf("\n----- MEMORY -----\n");
+	memory_init(vm, 10);
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'a');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'b');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'c');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'd');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'e');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'f');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'g');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'h');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'i');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'j');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_put(vm, 'k');
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	memory_pop(vm);
+	printf("\n== data ==> %s\n", vm->memory);
+	printf("== m_end ==> %s\n", vm->memory_end);
+	printf("== head  ==> %s\n", vm->head);
+	printf("== tail  ==> %s\n", vm->tail);
+	printf("== count ==> %d\n", vm->count);
+	printf("== size  ==> %d\n", vm->size);
 	return (0);
 }
