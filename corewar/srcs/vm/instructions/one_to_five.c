bd44c527e8b80bce909aa1a9733d515ac7b19e27
Arthur Dassier <arthur.dassier@epitech.eu>
2018-03-06 09:49:57 +0100

    added a lot of theory prototypes for instructions

--- corewar/srcs/vm/instructions/one_to_five.c
+++ corewar/srcs/vm/instructions/one_to_five.c
@@ -6,18 +6,42 @@
 */
 
 #include "virtual.h"
+#include "printf.h"
 
 void live(int player_number)
 {
-	my_printf("Le joueur %d (?) est en vie.", player_number)
+	my_printf("Le joueur %d (?) est en vie.", player_number);
+}
+
+void ld(int *param_one, int *register_one)
+{
+	(void) *param_one;
+	(void) *register_one;
+	//modif carry;
+	return;
+}
+
+void st(int *param_one, int *param_two)
+{
+	(void) *param_one;
+	(void) *param_two;
+	return;
 }
 
 void add(int *register_one, int *register_two, int *register_three)
 {
+	(void) *register_one;
+	(void) *register_two;
+	(void) *register_three;
+	//modif carry;
 	*register_three = *register_one + *register_two;
 }
 
 void sub(int *register_one, int *register_two, int *register_three)
 {
+	(void) *register_one;
+	(void) *register_two;
+	(void) *register_three;
+	//modif carry;
 	*register_three = *register_one - *register_two;
 }
--- corewar/srcs/vm/instructions/six_to_ten.c
+++ corewar/srcs/vm/instructions/six_to_ten.c
@@ -9,15 +9,41 @@
 
 void and(int *first, int *two, int *register_one)
 {
+	(void) *first;
+	(void) *two;
+	(void) *register_one;
+	//modif carry;
 	*register_one = *first & *two;
 }
 
 void or(int *first, int *two, int *register_one)
 {
+	(void) *first;
+	(void) *two;
+	(void) *register_one;
+	//modif carry;
 	*register_one = *first | *two;
 }
 
 void xor(int *first, int *two, int *register_one)
 {
+	(void) *first;
+	(void) *two;
+	(void) *register_one;
+	//modif carry;
 	*register_one = *first ^ *two;
+}
+
+void zjmp(int *index_one)
+{
+	(void) *index_one;
+	return;
+}
+
+void ldi(int *index_one, int *index_two, int *register_one)
+{
+	(void) *index_one;
+	(void) *index_two;
+	(void) *register_one;
+	return;
 }
