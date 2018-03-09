3b1943e5eaf8133576fcabd1453941301fa49bf3
Vincent <vincent.lemesle@epitech.eu>
2018-03-09 10:38:32 +0100

    add params error_handling

--- corewar/srcs/vm/recup_instruct/param_error.c
+++ corewar/srcs/vm/recup_instruct/param_error.c
@@ -6,6 +6,7 @@
 */
 
 #include "virtual.h"
+#include "op.h"
 
 static int recup_type(int types)
 {
@@ -17,25 +18,39 @@ static int recup_type(int types)
 		return (T_IND);
 	return (0);
 }
+
 static int or_types(int types)
 {
 	int	nb1 = 0;
 	int	nb2 = 0;
 	int	nb3 = 0;
 
-	nb1 = recup_type(types / 10);
+	nb1 = recup_type(types / 100);
 	nb2 = recup_type(types / 10 % 10);
 	nb3 = recup_type(types % 10);
 	return (nb1 | nb2 | nb3);
 }
 
+static int know_params_nb(int types)
+{
+	if (types % 10 != 0 && types / 10 % 10 != 0)
+		return (3);
+	if (types % 10 != 0)
+		return (2);
+	if (types / 100 != 0)
+		return (2);
+	return (1);
+}
+
 int is_good_params(instructions *list)
 {
 	int	ins_case = 0;
+	int	nb_params = 0;
 
 	ins_case = list->mnemonique[0] % 16 + 48;
 	ins_case = ins_case * 10 + list->mnemonique[1] % 16 + 48;
-	if (or_types(list->types) != op_tab[ins_case - 1].type[2])
+	nb_params = know_params_nb(list->types);
+	if (or_types(list->types) != op_tab[ins_case - 1].type[nb_params])
 		return (-1);
 	return (0);
 }
